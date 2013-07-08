cef190a210bcd779777a2d5121da4bf97a712918
Yunkis <dedy.sutomo@gmail.com>
2013-07-08 17:11:44 +0800

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ActiveRecord::Base
   #exercise 6.5
   before_save { email.downcase! }
   #before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -24,4 +25,10 @@ class User < ActiveRecord::Base
                     uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  after_validation { self.errors.messages.delete(:password_digest) }
+
+  private
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
